043f74de6818f365cc017bfb9b2e78828c0a371c
zaxizaxizaxi <zaxisnegzaxisnus@gmail.com>
2021-11-12 19:43:28 +0300

    safari commit

--- start/js/main.js
+++ start/js/main.js
@@ -12,4 +12,16 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>',
     });
+
+    $('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] 
+		},
+	});
 });
